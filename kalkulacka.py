ea88e80d0b2ed2a700beba88a0d67cf094f61497
kuri1o <tomas.kurial2009@gmail.com>
2026-06-11 16:25:17 +0200

    kompletně předělaný kód (match místo if) a přidány závorky (do budoucna upravit a zjednosušit)

--- kalkulacka.py
+++ kalkulacka.py
@@ -2,57 +2,129 @@ import math
 
 vysledek = input("cislo: ")
 
-if vysledek == "p":
-
+if vysledek == "pi":
         vysledek = math.pi
-
 else:
-
         vysledek = float(vysledek)
 
+
 while True:
-
-    operace = input("Operace (+ - * /): ")
-
-    #if operace == "()": závorka budouci
-
-    if operace == "=":
-        print(vysledek)
-        break
-
-    b = (input("cislo: "))
-
-    if b == "p":
-
-        b = math.pi
-
-    else:
-
-        b = float(b)
-
-
-
-    if operace == "+":
-        vysledek += b
-        print(vysledek)
-
-
-    elif operace == "-":
-        vysledek -= b
-        print(vysledek)
-
-
-    elif operace == "*":
-        vysledek *= b
-        print(vysledek)
-
-    elif operace == "/":
-        if b != 0:
-            vysledek /= b
+    operace = input("Operace [+, -, *, /, (, =]: ")
+    match operace:
+        case "+":
+            b = input("cislo: ")
+            if b == "pi":
+                b = math.pi
+            else:
+                b = float(b)
+            vysledek += b
             print(vysledek)
 
-        else:
-            print("Deleni nulou nejde")
+        case "-":
+            b = input("cislo: ")
+            if b == "pi":
+                b = math.pi
+            else:
+                b = float(b)
+            vysledek -= b
+            print(vysledek)
+
+        case "*":
+            b = input("cislo: ")
+            if b == "pi":
+                b = math.pi
+            else:
+                b = float(b)
+            vysledek *= b
+            print(vysledek)
+
+        case "/":
+            b = input("cislo: ")
+            if b == "pi":
+                b = math.pi
+            else:
+                b = float(b)
+            if b != 0:
+                vysledek /= b
+                print(vysledek)
+            else:
+                print("Deleni nulou nejde")
+        case "(":
+            c = input("cislo: ")
+            while True:
+                c = float(c)
+                operace = input("Operace [+, -, *, /, =]: ")
+                match operace:
+                    case "+":
+                        b = input("cislo: ")
+                        if b == "pi":
+                            b = math.pi
+                        else:
+                            b = float(b)
+                        c += b
+                        print(vysledek,"(",c,")")
+
+                    case "-":
+                        b = input("cislo: ")
+                        if b == "pi":
+                            b = math.pi
+                        else:
+                            b = float(b)
+                        c -= b
+                        print(vysledek,"(",c,")")
+
+                    case "*":
+                        b = input("cislo: ")
+                        if b == "pi":
+                            b = math.pi
+                        else:
+                            b = float(b)
+                        c *= b
+                        print(vysledek, "(", c, ")")
+
+                    case "/":
+                        b = input("cislo: ")
+                        if b == "pi":
+                            b = math.pi
+                        else:
+                            b = float(b)
+                        if b != 0:
+                            c /= b
+                            print(vysledek, "(", c, ")")
+                        else:
+                            print("Deleni nulou nejde")
+                    case ")":
+                        jak = input("jak to chces pripocitat?")
+                        match jak:
+                            case "+":
+                                vysledek+=c
+                                print(vysledek)
+                            case "-":
+                                vysledek-=c
+                                print(vysledek)
+                            case "*":
+                                vysledek*=c
+                                print(vysledek)
+                            case "/":
+                                if c != 0:
+                                    vysledek/=c
+                                    print(vysledek)
+                                else:
+                                    print("Deleni nulou nejde")
+                            case _:
+                                print("neplatne")
+                        break
+
+                    case _:
+                        print("neplatne")
+
+        case "=":
+            if operace == "=":
+                print("VYSLEDEK:",vysledek)
+            break
+
+        case _:
+            print("neplatne")
 
 
 
